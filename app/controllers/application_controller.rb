8311c994c706358300caf85424ba82f64c4fd4f4
Otávio Pace <otaviopp8@gmail.com>
2018-09-04 00:19:54 -0300

    users: add username

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :passord, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:username, :email, :passord, :password_confirmation, :remember_me])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :passord, :password_confirmation])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root 'pages#index'
 
-  get 'home' => 'pages#home'
-  get 'profile' => 'pages#profile'
-  get 'explore' => 'pages#explore'
+  get '/home' => 'pages#home'
+  get '/user/:id' => 'pages#profile'
+  get '/explore' => 'pages#explore'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
